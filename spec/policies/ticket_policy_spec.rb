dec60baff5d5b51c61fed66203c6fe7b6d0f71d3
Brad <taoquan584@gmail.com>
2017-01-24 23:21:38 -0500

    Only managers and admins can delete tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
 
     context "for viewers of the project" do
@@ -19,6 +20,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
 
     context "for editors of the project" do
@@ -26,6 +28,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
 
       context "when the editor created the ticket" do
         before { ticket.author = user }
@@ -38,6 +41,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should permit_action :create }
       it { should permit_action :update }
+      it { should permit_action :destroy }
     end
 
     context "for managers of other projects" do
@@ -47,6 +51,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
 
     context "for administrators" do
@@ -54,6 +59,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should permit_action :create }
       it { should permit_action :update }
+      it { should permit_action :destroy }
     end
   end
 end
